c8940828fb4002c7d2674f4582aeea46567a0b33
riverosmejia <113913659+riverosmejia@users.noreply.github.com>
2024-02-18 18:34:08 -0500

    Break my Strike

--- last train to london/main.cpp
+++ last train to london/main.cpp
@@ -2,330 +2,86 @@
 #include <iostream>
 #include <algorithm>
 
-/*Time!
-Ten
-Nine
-Eigth
-Seven
-Six
-Five
-Four
-Three
-Two
-One
-Go!
-Buon anno a tutti!
-¡Feliz año nuevo a todos!
-Felis anho novo!
-Bonne anne!
-Buon anno Italia!
-Happy new year everybody!
-Burn, baby burn,
-Burn, baby burn,
-Burn, baby burn,
-Burn, baby burn.
-Burning
-To my surprise,
-One hundred stories high
-People getting loose now,
-Getting down on the roof
-I heard somebody say
-(Burn baby burn) Disco Inferno
-(Burn baby burn) I Burn that mother down
-(Burn baby burn) Disco Inferno
-(Burn baby burn) I Burn that mother down... Burnin'
-I never can say goodbye,
-No no no I...
-I never can say goodbye
-Everytime I think I've had enough
-And start heading for the door
-There's a very strange vibration
-Piercing me right to the core
-It says
-Turn around you fool
-You know you love him more and more
-Tell me why (tell me why)
-Is it so
-Don't wanna let you go
-I never can say goodbye Boy
-Uooh baby
-I never can say goodbye
-No, no, no, no, no, no, no, no, no, uh uh
-Su la mani cosi, yahoo!
-A tiamo E siame celebration
-Yahoo!
-This is your celebration
-Celebrate good times, come on! (Let's celebrate)
-Celebrate good times, come on! (Let's celebrate)
-It's time to come together
-It's up to you,
-What's your pleasure
-Everyone around the world
-Come on!
-Relight my fire,
-Your love is my only desire
-Relight my fire
-Cos I need your love
-Baby,
-Do you understand me now?
-Sometimes I feel a little mad
-But, don't you know that no one alive can always be an angel
-When things go wrong I seem to be bad
-'Cause I'm just a soul whose intentions are good
-Oh Lord, please don't let me be misunderstood
-Oh! Oh! Ooh! Yeah! yeah! yeah! uh! uh!
-Aaahh Freak out!
-Le Freak, c'est chic
-Freak out!
-Aaahh Freak out! Uh!
-Le Freak, c'est chic
-Freak out!
-Have you heard about the new dance craze?
-Listen to us, I'm sure you'll be amazed
-Big fun to be had by everyone
-It's up to you, It surely can be done
-Aaahh Freak out!
-Le Freak, c'est chic
-Freak out!
-Oh we are family
-I got all my sisters with me, oh oh
-We are family, we are family
-Get up everybody and sing
-We are family, we are family
-I got all my sisters with me oh oh
-We are family, oh yeah
-Get up everybody and sing
-Oh!
-I come home
-In the morning light
-My mother says
-When you gonna live
-Your life right
-Oh mother dear
-We're not
-The fortunate ones
-And girls
-They wanna have fun
-Oh girls Just wanna have fun
-Tutti l'girls
-They want wanna have fun
-Girls
-Wanna have
-They just wanna
-They just wanna
-They just wanna
-They just wanna
-They just wanna
-They just wanna
-They just wanna
-Girls just wanna have fun
-They just to wanna
-Oh oh oh oh oh oh oh
-Oh the night is my world
-City life painted girls
-In a day nothing matters
-It's the night time that flatters
-You take my self you take my self control
-You got me livin' only for the night
-Before the morning comes the story's told
-You take my self you take my self control
-I I live among the creatures of the night
-I haven't got the will to try and fight against a new tomorrow
-So I guess i just believe it that tomorrow never knows
-Jungle life
-You're far away from nothing
-It's all right
-You won't miss home
-Take a chance
-Leave everything behind you
-Come and join me Won't be sorry
-It's easy to survive
-Jungle life We're living in the open
-All alone like Tarzan Boy
-Hide and seek
-We play along while rushing cross the
-Forest Monkey business on a sunny afternoon
-Oh, oh, oh, oh, oh, oh, oh, oh, oh, oh, oh
-Oh, oh, oh, oh, oh, oh, oh, oh, oh, oh, oh
-Oh, oh, oh, oh, oh, oh, oh, oh, oh, oh, oh
-Navighiamo già da un po',
-Bene o male non lo so,
-Stella guida gli occhi tuoi,
-Un amore grande
-Noi peschiamo nella fantasia
-Pietre verdi di Bahia?
-Al timone la follia
-E ci ritroviamo, in alto mare
-In alto mare
-Per poi lasciarsi andare
-Sull'onda che ti butta giù
-E poi ti scaglia verso il blu
-E respirare
-In alto mare
-Come due uccelli da ammazzare
-Oh, oh, oh, oh, oh. Oho, hu hu, hu hu, hu!
-Downtown's been caught by the hysteria, People scream and shout
-A generation's on the move
-When disco spreads like a bacteria
-These lonely days are out
-Welcome the passion of the groove
-The golden years, The silver tears
-You wore a tie like Richard Gere
-I wanna get down You spin me around
-I stand on the borderline, Crying at the discoteque
-Oh oh oh Crying at the discoteque oh oh hu!
-I saw you crying
-I saw you crying at the discoteque
-I saw you crying
-I saw you crying at the discoteque
-Goddess on the mountain top
-Burning like a silver flame
-The summit of beauty and love
-And Venus was her name
-She's got it
-Yeah, baby, she's got it
-I'm your Venus, I'm your fire At your desire
-Well, I'm your Venus, I'm your fire At your desire, hoo!
-Sittin' here eatin' my heart out waitin'
-Waitin' for some lover to call
-Dialed about a thousand numbers lately
-Almost rang the phone off the wall
-Lookin' for some hot stuff baby this evenin'
-I need some hot stuff baby tonight
-I want some hot stuff baby this evenin'
-Gotta have some hot stuff
-Gotta have some love tonight
-I need hot stuff
-I don't know
-Why you're not there
-I give you my love, but you don't care
-So what is right,
-And what is wrong Gimme a sign!
-What is love?
-Baby don't hurt me,
-Don't hurt me,
-No more
-What is love?
-Baby don't hurt me,
-Don't hurt me,
-No more
-Uoh uoh uoh uoh
-Uoh uoh uoh uoh
-Oh!
-Uoh uoh uoh uoh
-Uoh uoh uoh
-Ooooooooh! (This is the rhythm of the night)
-The night, oh yeah
-The rhythm of the night
-This is the rhythm of my life
-My life, oh yeah
-The rhythm of my life
-Won't you teach me how to love and learn
-There'll be nothing left for me to yearn
-Think of me and burn, and let me hold your hand, hoo!
-I don't wanna face the world in tears
-Please think again, I'm on my knees
-Sing that song to me, no reason to repent
-I know you wanna say it
-This is the rhythm of the night
-The night, ancora, oh yeah, cosi!
-The rhythm of the night
-This is the rhythm of my life
-My life, oh yeah
-The rhythm of my life, life, life
-Na na na na na na
-Na na na na na na
-Rumore rumore eh eh
-Na na na na na na
-Na na na non mi sento sicura, sicura,
-Sicura mai
-Na na na na na na
-Io stasera vorrei
-Tornare indietro con il tempo
-Na na
-E ritornare al tempo Che c'eri tu
-Però perciò Che non pensarci più su
-Ma ritornare, ritornare perchè
-Quando ho deciso che facevo da me!
-Cuore, batticuore
-Na na na na na
-Mi è sembrato di sentire un rumore, rumore
-Eh Sera da paura
-Io da sola non mi sento sicura, sicura
-Sicura mai
-Cosi cosi cosi cosi!
-We were born to be,
-Alive
-We were born to be,
-Alive
-Born,
-Born
-Born to be alive
-(Born to be alive)
-Yes we were born,
-Born,
-Born,
-Born... to be alive
-People ask me why I never find a place to stop and settle
-Down down down
-We were born to be alive
-A suitcase and an old guitar and something new to occupy my mind mind mind
-Yes we were born to be alive
-Yes we were born,
-Born born to be alive
-(Born to be alive)
-Yes we were born
-Born
-Born
-Born to be alive
-It's good to be alive
-It's good to be alive
-It's good to be alive
-It's good to be alive
-And we were born
-To be alive
-And we were born
-To be alive
-To be alive
-Cosi?
-All the old paintings on the tombs
-They do the sand dance don't you know
-If they move too quick
-(Oh whey oh)
-They're falling down like a domino
-All the bazaar men by the Nile
-They got the money on a bet
-Gold crocodiles
-(Oh whey oh)
-They snap their teeth on your cigarette
-Foreign types with the hookah pipes say Ay oh whey oh, oy oh whey oh
-Walk like an Egyptian, Cosi
-Walk like an Egyptian
-Su, su l'mani
-I got chills, they're multiplyin',
-And I'm losin' control
-Cause the power you're supplyin', it's electrifyin'
-You better shape up,
-Cause I need a man,
-To my heart I must be true
-Nothing left, nothing left for me to do
-You're the one that I want
-(You are the one I want),
-Ooh ooh ooh, honey!
-The one that I want
-(You are the one I want),
-Ooh ooh ooh, honey
-The one I need (the one I need),
-Oh yes indeed (yes indeed)
-It's fun to stay at the YMCA
-It's fun to stay at the YMCA
-They have everything for you men to enjoy,
+/*Young man, there's no need to feel down
+I said: Young man, pick yourself off the ground
+I said: Young man, 'cause you're in a new town
+There's no need to be unhappy
+
+Young man, there's a place you can go
+I said: Young man, when you're short on your dough
+You can stay there, and I'm sure you will find
+Many ways to have a good time
+
+It's fun to stay at the Y-M-C-A
+It's fun to stay at the Y-M-C-A
+
+They have everything
+For you men to enjoy
 You can hang out with all the boys
-YMCA
-It's fun to stay at the YMCA
-You can get yourself cleaned, you can have a good meal,
+
+It's fun to stay at the Y-M-C-A
+It's fun to stay at the Y-M-C-A
+
+You can get yourself cleaned
+You can have a good meal
 You can do whatever you feel
-It's fun to stay at the YMCA
+
+Young man, are you listening to me?
+I said: Young man, what do you want to be?
+I said: Young man, you can make real your dreams
+But you got to know this one thing
+
+No man does it all by himself
+I said: Young man, put your pride on the shelf
+And just go there, to the Y-M-C-A
+I'm sure they can help you today
+
+It's fun to stay at the Y-M-C-A
+It's fun to stay at the Y-M-C-A
+
+They have everything
+For you men to enjoy
+You can hang out with all the boys
+
+It's fun to stay at the Y-M-C-A
+It's fun to stay at the Y-M-C-A
+
+You can get yourself cleaned
+You can have a good meal
+You can do whatever you feel
+
+Young man, I was once in your shoes
+I said: I was down and out with the blues
+I felt no man cared if I were alive
+I felt the whole world was so tight
+
+That's when someone came up to me
+And said: Young man, take a walk up the street
+There's a place there called the Y-M-C-A
+They can start you back on your way
+
+It's fun to stay at the Y-M-C-A
+It's fun to stay at the Y-M-C-A
+
+They have everything
+For you men to enjoy
+You can hang out with all the boys
+
+Y-M-C-A
+It's fun to stay at the Y-M-C-A
+
+Young man, young man
+There's no need to feel down
+Young man, young man
+Get yourself off the ground
+
+Y-M-C-A
+And just go to the Y-M-C-A
+
+Young man, young man
+There's no need to feel down
+Young man, young man
+Get yourself off the ground
 */
 
 int findOriginalLength(int parts[], int n) {
